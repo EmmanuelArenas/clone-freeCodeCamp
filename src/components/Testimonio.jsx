02015b55327e42ef6980127ec5588022f8622f12
EmmanuelArenas <emmanuel.live19@gmail.com>
2022-09-13 20:16:41 -0500

    componente testimonio

--- src/components/Testimonio.jsx
+++ src/components/Testimonio.jsx
@@ -8,8 +8,20 @@ const Testimonio = () => {
         src={require("../image/testimonio-emma.png")}
         alt="Foto de Emma"
       />
+      <div className="contenedor-text-testimonio">
+        <p className="nombre-testimonio">Emma Bostina en Suecia</p>
+        <p className="cargo-testimonio">Ingeniera de Software en Spotify</p>
+        <p className="texto-testimonio">
+          "Siempre he tenido problemas para aprender JavaScript. He tomado
+          muchos cursos, pero el curso de freeCodeCamp fue el que se quedó.
+          Estudiar JavaScript, así como estructuras de datos y algoritmos en
+          freeCodeCamp me dio las habilidades y la confianza que necesitaba para
+          conseguir el trabajo de mis sueños como ingeniero de software en
+          Spotify."
+        </p>
+      </div>
     </div>
   );
 };
 
-module.exports = { Testimonio };
+export default Testimonio; //exportación por defecto
