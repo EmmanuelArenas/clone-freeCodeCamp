66054da044afc60930d9645df5ca04e3b58f1b40
EmmanuelArenas <emmanuel.live19@gmail.com>
2022-09-13 20:05:16 -0500

    elimine lo que no ocuparia para el proyecto

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"; //No permite trabajar con componenetes
+import ReactDOM from "react-dom/client"; // nos permite interactuar con el DOM
+import "./index.css";
+import App from "./App"; //importa componente principal para poder usarlo
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
@@ -14,4 +13,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals(); //Mide el desempeño de la apliccion
