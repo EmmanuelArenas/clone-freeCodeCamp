cc3316e11c41e7a2b964c607c2b4950b42b7a5d4
EmmanuelArenas <emmanuel.live19@gmail.com>
2022-09-15 23:59:20 -0500

    props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import "./App.css";
 import Testimonio from "./components/Testimonio.jsx";
-
+import { Personas } from "./utils/personas.js";
 function App() {
+  // console.log(Personas);
   return (
     <div className="App">
-      <div class="contenedor-principal">
+      <div className="contenedor-principal">
         <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
-        <Testimonio />
+        {Personas &&
+          Personas?.map((e, index) => (
+            <Testimonio
+              key={index}
+              nombre={e.nombre}
+              pais={e.pais}
+              imagen={e.imagen}
+              cargo={e.cargo}
+              empresa={e.empresa}
+              testimonio={e.testimonio}
+            />
+          ))}
       </div>
     </div>
   );
